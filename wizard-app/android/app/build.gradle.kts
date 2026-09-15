a188b2405949746057c7278e81df0376eabc6a07
kbosk <kostyaboskin@gmail.com>
2026-09-16 00:05:33 +0300

    Claude updates of the app, onboarding, styleguide, etc

--- wizard-app/android/app/build.gradle.kts
+++ wizard-app/android/app/build.gradle.kts
@@ -17,10 +17,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.bargain.wiz"
@@ -66,4 +62,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+    }
 }
